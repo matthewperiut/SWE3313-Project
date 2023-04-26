3b6914e1570226b7a7b9c469171ce802702060aa
Matthew Periut <slaincow@gmail.com>
2023-04-26 19:46:56 -0400

    Tables Scene

---
+++ Assets/Scripts/TableUsable.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class TableUsable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField] private string code;
+    [SerializeField] private Button button;
+
+    private string filePath;
+    
+    private static Color red = new Color(1f, 0f, 0f, 1f);
+    private static Color green = new Color(0f, 1f, 0f, 1f);
+    private static Color yellow = new Color(1f, 1f, 0f, 1f);
+
+    void Start()
+    {
+        filePath = Path.Combine(Application.persistentDataPath, "user.txt");
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            // Read the data from the file
+            string data = reader.ReadToEnd();
+            if (data.Contains(code))
+            {
+                button.interactable = true;
+            }
+        }
+
+        int randomInt = Random.Range(0, 3);
+        if (randomInt == 0)
+            button.image.color = red;
+        if (randomInt == 1)
+            button.image.color = green;
+        if (randomInt == 2)
+            button.image.color = yellow;
+    }
+}
